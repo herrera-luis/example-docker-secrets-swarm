3ed31c087f372cf58c5baf7e7d2f006718c1e317
luisherreratw <lherrera@thoughtworks.com>
2018-12-30 15:12:04 -0500

    added comments

--- script.sh
+++ script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 docker swarm init
 
-docker secret create pg-password ./pg-password
+docker secret create pg-password ./pg-password # we create the secret variable pg-password with the value of the file pg-password (the_password)
 docker secret create pg-username ./pg-username
 docker secret create pg-bdd ./pg-bdd
 
 docker network create -d overlay my-lab
 
 docker service create --name pg-test-secret --network my-lab \
-            --secret pg-password --secret pg-username --secret pg-bdd \
-            -e POSTGRES_PASSWORD_FILE="/run/secrets/pg-password" \
+            --secret pg-password --secret pg-username --secret pg-bdd \ # sends the secrets variables to the container
+            -e POSTGRES_PASSWORD_FILE="/run/secrets/pg-password" \ # access to the pg-password secret variable
             -e POSTGRES_USER_FILE="/run/secrets/pg-username" \
             -e POSTGRES_DB_FILE="/run/secrets/pg-bdd" postgres
 
-docker service create -p 99:80 --name=pgadmin --network my-lab \
+docker service create -p 99:80 --name=pgadmin --network my-lab \ # we raise this service just for connect to the database via web
             -e PGADMIN_DEFAULT_EMAIL=user@domain.com \
             -e PGADMIN_DEFAULT_PASSWORD=supersecret \
             -d dpage/pgadmin4
