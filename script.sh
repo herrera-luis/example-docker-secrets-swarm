0d2394d2c41d1fbaf880eb3c1679c423cf105a23
luisherreratw <lherrera@thoughtworks.com>
2019-01-06 21:10:20 -0500

    fixed error in script file

--- script.sh
+++ script.sh
@@ -7,13 +7,15 @@ docker secret create pg-bdd ./pg-bdd
 
 docker network create -d overlay my-lab
 
+# sends the secrets variables to the container
+# access to the pg-password secret variable
 docker service create --name pg-test-secret --network my-lab \
-            --secret pg-password --secret pg-username --secret pg-bdd \ # sends the secrets variables to the container
-            -e POSTGRES_PASSWORD_FILE="/run/secrets/pg-password" \ # access to the pg-password secret variable
+            --secret pg-password --secret pg-username --secret pg-bdd \
+            -e POSTGRES_PASSWORD_FILE="/run/secrets/pg-password" \
             -e POSTGRES_USER_FILE="/run/secrets/pg-username" \
             -e POSTGRES_DB_FILE="/run/secrets/pg-bdd" postgres
 
-docker service create -p 99:80 --name=pgadmin --network my-lab \ # we raise this service just for connect to the database via web
+docker service create -p 99:80 --name=pgadmin --network my-lab \
             -e PGADMIN_DEFAULT_EMAIL=user@domain.com \
             -e PGADMIN_DEFAULT_PASSWORD=supersecret \
             -d dpage/pgadmin4
